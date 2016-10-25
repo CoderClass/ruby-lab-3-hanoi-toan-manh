fcc22716a2134cd674642fe1c7234db76fea7a2f
Manh T. Truong <manhattan.vn@gmail.com>
2016-10-25 21:15:40 +0700

    Milestone 5

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,21 @@ class Product < ActiveRecord::Base
   def self.alphabetical
     order('name')
   end
+
+  def discount_amount
+    case
+    when price_vnd > 800000
+      price_vnd * 0.41
+    when price_vnd > 200000
+      price_vnd * 0.31
+    when price_vnd > 100000
+      price_vnd * 0.21
+    else
+      0
+    end
+  end 
+
+  def final_price
+    (price_vnd - discount_amount).round(-4)
+  end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,4 +6,41 @@ RSpec.describe Product, type: :model do
     @product3 = Product.create(name: 'Tablet')
     expect(Product.alphabetical).to eq [@product2,@product1,@product3]
   end
+
+  describe "On Sale" do
+
+    describe "Discount amount" do
+      it "Cost more than 100K VND should discount 21%" do
+        @product = Product.create! price_vnd: 120000
+        expect(@product.discount_amount).to eq 25200 
+      end
+
+      it "Cost more than 200K VND should discount 31%" do
+        @product = Product.create! price_vnd: 220000
+        expect(@product.discount_amount).to eq  68200
+      end
+      it "Cost more than 800K VND should discount 41%" do
+        @product = Product.create! price_vnd: 820000
+        expect(@product.discount_amount).to eq 336200 
+      end
+      it "Cost less than 100K VND should discount 0%" do
+        @product = Product.create! price_vnd: 20000
+        expect(@product.discount_amount).to eq 0
+      end
+    end
+
+    describe "Finale price should round to 10K" do
+
+      it "Cost 115K after discount, final price shoud be 120K" do
+        @product = Product.create! price_vnd: 146000
+        expect(@product.final_price).to eq 120000
+      end
+      it "Cost 114K after discount, final price shoud be 110K" do
+        @product = Product.create! price_vnd: 144000
+        expect(@product.final_price).to eq 110000
+      end
+
+    end
+  end
+
 end
